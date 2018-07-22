77ddce7b4136472282333e1f7ba6c819ed669914
Boris Belousov <belousov@ias.informatik.tu-darmstadt.de>
2018-07-22 18:17:24 +0200

    Minimum viable interface for Qube

---
+++ common.py
@@ -0,0 +1,75 @@
+import socket
+import struct
+import numpy as np
+from scipy import signal
+
+
+class QSocket:
+    """
+    Class for communication with Quarc.
+    """
+    def __init__(self, ip, x_len, u_len):
+        """
+        Prepare socket for communication.
+        :param ip: IP address of the Windows PC
+        :param x_len: number of measured state variables to receive
+        :param u_len: number of control variables to send
+        """
+        self._x_fmt = '>' + (x_len + 1) * 'd'  # +1 is for the timestamp
+        self._u_fmt = '>' + u_len * 'd'
+        self._buf_size = (x_len + 1) * 8  # 8 bytes for each double
+        self._port = 9095  # fixed in Simulink model
+        self._soc = socket.socket()
+        self._soc.connect((ip, self._port))
+
+    def snd_rcv(self, u):
+        """
+        Send u and receive (t, x).
+        :param u: control vector
+        :return: t, x: timestamp, vector of measured states
+        """
+        self._soc.send(struct.pack(self._u_fmt, u))
+        data = self._soc.recv(self._buf_size)
+        q = struct.unpack(self._x_fmt, data)
+        t = q[0]
+        x = np.array(q[1:])
+        return t, x
+
+
+class SymmetricBoxSpace:
+    """
+    Generic real box space with symmetric boundaries.
+    """
+    def __init__(self, bound, labels):
+        assert isinstance(bound, np.ndarray)
+        assert bound.size == len(labels)
+        self.bound_lo = -bound
+        self.bound_up = bound
+        self.dim = self.bound_lo.size
+        self.labels = labels
+
+    def project(self, ele):
+        assert isinstance(ele, np.ndarray)
+        return np.clip(ele, self.bound_lo, self.bound_up)
+
+
+class VelocityFilter:
+    """
+    Create discrete velocity filter from continuous one.
+    """
+    def __init__(self, num=(50, 0), den=(1, 50), dt=0.002):
+        """
+        Initialize discrete filter coefficients
+        :param num: continuous-time filter numerator
+        :param den: continuous-time filter denominator
+        :param dt: sampling time interval
+        """
+        derivative_filter = signal.cont2discrete((num, den), dt)
+        self.b = derivative_filter[0].ravel()
+        self.a = derivative_filter[1]
+        self.z = np.zeros((1, self.b.size))
+
+    def __call__(self, x):
+        xd, self.z = signal.lfilter(self.b, self.a, x[None, :], axis=0,
+                                    zi=self.z)
+        return xd.ravel()
---
+++ qube.py
@@ -0,0 +1,38 @@
+from common import QSocket, SymmetricBoxSpace, VelocityFilter
+import numpy as np
+
+
+class Qube:
+    def __init__(self, ip="130.83.164.122"):
+        # theta - arm angle; alpha - pendulum angle; (0, 0) = down position
+        self.measurement_space = SymmetricBoxSpace(
+            bound=np.array([2.3, np.inf]),
+            labels=('theta', 'alpha')
+        )
+        # s = [theta, alpha, theta_dot, alpha_dot]
+        self.state_space = SymmetricBoxSpace(
+            bound=np.array([2.3, np.inf, np.inf, np.inf]),
+            labels=('theta', 'alpha', 'theta_dot', 'alpha_dot')
+        )
+        # a = motor voltage
+        self.action_space = SymmetricBoxSpace(
+            bound=np.array([5.0]),
+            labels=('motor_voltage',)
+        )
+        # Initialize velocity filter
+        self._filt = VelocityFilter()
+        # Initialize communication
+        self._soc = QSocket(ip, self.measurement_space.dim,
+                            self.action_space.dim)
+
+    def step(self, a):
+        """Send command and receive next state."""
+        t, x = self._soc.snd_rcv(self.action_space.project(a))
+        s = np.r_[x, self._filt(x)]
+        return t, s
+
+
+if __name__ == "__main__":
+    qube = Qube()
+    t, s = qube.step(np.array([0.0]))
+    print(t, s)
