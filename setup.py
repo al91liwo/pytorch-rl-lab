a2c6b748e017600776d8f4dce969cee8ff8c93a8
b4be1 <b.a.belousov@gmail.com>
2018-09-26 11:13:44 +0200

    v0.0.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='quanser_robots',
-      version='0.0.3',
+      version='0.0.4',
       description='Simulated environments and Quanser robot control interfaces',
       url='https://git.ias.informatik.tu-darmstadt.de/quanser/clients',
       author='Intelligent Autonomous Systems Lab',
@@ -10,6 +10,6 @@ setup(name='quanser_robots',
       packages=['quanser_robots', 'quanser_robots.qube'],
       zip_safe=False,
       install_requires=[
-          'gym>=0.10.5', 'numpy>=1.15.1', 'scipy>=1.1.0', 'matplotlib>=2.2.3',
+          'gym>=0.10.5', 'numpy>=1.14.5', 'scipy>=1.1.0', 'matplotlib>=2.2.3',
           'vpython>=7.4.6'
       ])
