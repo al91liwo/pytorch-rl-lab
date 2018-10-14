Fix a bug in log_probs computation

quanser_robots/qube/examples/ppo.py
@@ -100,7 +100,8 @@ class Policy:
         self._nb_epochs = nb_epochs
         self._batch_size = batch_size
         self._e_clip = e_clip
-        self._ent_const = a_dim * (0.5 + 0.5 * np.log(2 * np.pi))
+        self._log_prob_const = 0.5 * a_dim * np.log(2 * np.pi)
+        self._ent_const = 0.5 * a_dim + self._log_prob_const
         self._mu = Net((s_dim, *hl_size, a_dim))
         self._log_scale = torch.tensor(a_dim * [np.log(sig0)],
                                        requires_grad=True,
@@ -122,10 +123,8 @@ class Policy:
         return self._log_scale.sum() + self._ent_const
 
     def log_probs(self, obs: torch.Tensor, act: torch.Tensor):
-        loc = self._mu(obs)
-        var = torch.exp(self._log_scale) ** 2
-        log_probs = -((act - loc) ** 2) / (2 * var) \
-                    - self._log_scale - np.log(np.sqrt(2 * np.pi))
+        log_probs = -0.5*((act-self._mu(obs))**2)*torch.exp(-2*self._log_scale)\
+                    -self._log_scale.sum() - self._log_prob_const
         return log_probs.sum(dim=1, keepdim=True)
 
     def _loss(self, log_probs, log_probs_old, adv):