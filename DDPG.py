95be44e4762b7e44024723cdfdba6434bd7fd68a
Frederik Wegner <onlyknoppsa@gmail.com>
2018-12-21 03:16:57 +0100

    Training and Trial are now separated. Trains with as many batches as trial had steps. trail and training are refectored into separate functions.

--- DDPG.py
+++ DDPG.py
@@ -2,8 +2,8 @@ import copy
 import torch
 from torch import nn
 import numpy as np
-from pympler import summary, muppy
 import gc
+import time
 
 from ActorNetwork import ActorNetwork
 from CriticNetwork import CriticNetwork
@@ -13,15 +13,15 @@ gc.enable()
 
 class DDPG:
     
-    def __init__(self, env, buffer_size=1000000, batch_size=64,
-                 epsilon=.99, tau=1e-2, episodes=100, warmup_samples=10000,
-                 transform= lambda x : x, actor_lr=1e-4, critic_lr=1e-3):
+    def __init__(self, env, buffer_size=100000, batch_size=64,
+                 epsilon=.99, tau=1e-2, episodes=50, warmup_samples=5000,
+                 transform= lambda x : x, actor_lr=1e-3, critic_lr=1e-3, max_iteration_time=0.01, noise_decay=1):
         self.env = env
         self.state_dim = self.env.observation_space.shape[0]
         self.action_dim = self.env.action_space.shape[0]
 
-        actor_param = [self.state_dim, 100, 200, 300, 300, self.action_dim]
-        critic_param = [self.state_dim + self.action_dim, 100, 200, 300, 300, 1]
+        actor_param = [self.state_dim, 100, 200, 300, 400, self.action_dim]
+        critic_param = [self.state_dim + self.action_dim, 100, 200, 300, 400, 1]
         self.actor_network = ActorNetwork(actor_param)
         self.critic_network = CriticNetwork(critic_param)
         self.actor_target = ActorNetwork(actor_param)
@@ -34,7 +34,7 @@ class DDPG:
         
         self.replayBuffer = ReplayBuffer(buffer_size)
         self.batch_size = batch_size
-        self.n_batches = warmup_samples
+        self.warmup_samples = warmup_samples
 
         self.epsilon = epsilon
 
@@ -42,7 +42,9 @@ class DDPG:
         self.episodes = episodes
 
         self.noise_warmup = torch.distributions.normal.Normal(0, self.env.action_space.high[0])
-        self.noise_training = torch.distributions.normal.Normal(0, self.env.action_space.high[0]/5)
+        self.noise_training = torch.distributions.normal.Normal(0, self.env.action_space.high[0]/10)
+        self.noise_decay = noise_decay
+
         self.transformObservation = transform
 
     def action_selection(self, state):
@@ -52,7 +54,7 @@ class DDPG:
         return: action with highest reward
         """
         self.actor_target.eval()
-        action = self.actor_target(state)
+        action = self.actor_target(state.type(torch.float32))
         self.actor_target.train()
         return action
 
@@ -75,62 +77,69 @@ class DDPG:
         loss.backward(retain_graph=False)
         self.critic_optim.step()
 
+    def update(self):
+        sample_batch = self.replayBuffer.sample_batch(self.batch_size)
+        s_batch, a_batch, r_batch, s_2_batch = sample_batch
+
+        # calculate value/critic loss
+        next_action = self.actor_target(s_2_batch)
+        critic_target_prediction = self.critic_target(s_2_batch, next_action)
+        expected_critic = r_batch + self.epsilon * critic_target_prediction
+
+        critic_pred = self.critic_network(s_batch, a_batch)
+        critic_loss = self.loss(critic_pred, expected_critic)
+        self.update_critic(critic_loss)
+
+        # calculate policy/actor loss
+        actor_loss = self.actor_network(s_batch)
+        actor_loss = - self.critic_network(s_batch, actor_loss)
+        actor_loss = torch.mean(actor_loss)
+        self.update_actor(actor_loss)
+
+        self.softUpdate(self.critic_network, self.critic_target)
+        self.softUpdate(self.actor_network, self.actor_target)
+
+        return critic_loss, actor_loss
+
+    def trial(self, epoch):
+        total_reward = 0
+        obs = self.env.reset()
+        done = False
+        trial_len = 0
+        while not done:
+            start = time.time()
+            state = self.transformObservation(obs)
+
+            action = self.action_selection(torch.squeeze(torch.tensor(state)))
+            noise_function = self.noise_training if self.replayBuffer.count >= self.warmup_samples else self.noise_warmup
+            action = noise_function.sample((self.action_dim,)) + action
+
+            obs, reward, done, _ = self.env.step(np.array([action.item()]))
+            next_state = self.transformObservation(obs)
+
+            total_reward += reward
+            trial_len += 1
+            if epoch > 30:
+                self.env.render()
+            # for continuity in replay buffer the next_state should should be a tensor
+            self.replayBuffer.add(state, action, reward, next_state)
+        return trial_len, total_reward
+
     def train(self):
         print("Training started...")
         i = 0
         while i < self.episodes:
-            total_reward = 0
-            obs = self.env.reset()
-            done = False
-            step_i = 0
+            trial_len, total_reward = self.trial(i)
+            print(i, "/", self.episodes)
+            if self.replayBuffer.count >= self.warmup_samples:
+                i += 1
+                for _ in range(trial_len):
+                    critic_loss, actor_loss = self.update()
 
-            print(i, self.episodes)
-            while (not done):
-                state = self.transformObservation(obs)
+                self.noise_training = torch.distributions.normal.Normal(0, self.env.action_space.high[0]/10*self.noise_decay**i)
 
-                action = self.action_selection(torch.squeeze(torch.tensor(state)))
-                noise_function = self.noise_training if self.replayBuffer.count >= self.n_batches else self.noise_warmup
-                action = noise_function.sample((self.action_dim,)) + action
-
-
-                obs, reward, done, _ = self.env.step(np.array([action.item()]))
-                next_state = self.transformObservation(obs)
-
-
-                total_reward += reward
-
-
-                # for continuity in replay buffer the next_state should should be a tensor
-                self.replayBuffer.add(state, action, reward, next_state)
-                if self.replayBuffer.count >= self.n_batches:
-                        step_i = 1
-                        sample_batch = self.replayBuffer.sample_batch(self.batch_size)
-                        s_batch, a_batch, r_batch, s_2_batch = sample_batch
-
-                        # calculate value/critic loss
-                        next_action = self.actor_target(s_2_batch)
-                        critic_target_prediction = self.critic_target(s_2_batch, next_action)
-                        expected_critic = r_batch + self.epsilon * critic_target_prediction
-
-                        critic_pred = self.critic_network(s_batch, a_batch)
-                        critic_loss = self.loss(critic_pred, expected_critic)
-                        self.update_critic(critic_loss)
-
-                        # calculate policy/actor loss
-                        actor_loss = self.actor_network(s_batch)
-                        actor_loss = - self.critic_network(s_batch, actor_loss)
-                        actor_loss = torch.mean(actor_loss)
-                        self.update_actor(actor_loss)
-
-                        self.softUpdate(self.critic_network, self.critic_target)
-                        self.softUpdate(self.actor_network, self.actor_target)
-
-
-            i += step_i
-
-            if self.replayBuffer.count >= self.n_batches:
-                print("critic loss: ", critic_loss)
-                print("actor_loss: ", actor_loss)
+                print("Crititc loss:", critic_loss)
+                print("Actor loss:", actor_loss)
                 print(self.replayBuffer.count)
                 gc.collect()
                 # all_objects = muppy.get_objects()
--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import numpy as np
 
 from DDPG import DDPG
 
-env = gym.make("Qube-v0")
+env = gym.make("Pendulum-v0")
 
-ddpg = DDPG(env=env, episodes=50, transform=lambda obs: obs, warmup_samples=10000)
+ddpg = DDPG(env=env, episodes=50, transform=lambda obs: obs, warmup_samples=4000)
 
 ddpg.train()
 ddpg.actor_target.eval()
